3193c95b9886063fd35ed55f5b52a8c0d9e204e7
altvie <xyzul29@gmail.com>
2025-07-15 01:19:14 +0800

    feat: bot presence

--- src/events/ready.js
+++ src/events/ready.js
@@ -5,5 +5,16 @@ module.exports = {
   once: true,
   execute(client) {
     console.log(`${client.user.tag} successfuly logged in!`)
+
+    // Set bot presence
+    client.user.setPresence({
+      activities: [
+        {
+          name: 'Sanbi JS',
+          type: 3 // Watching
+        }
+      ],
+      status: 'online'
+    })
   }
 }
